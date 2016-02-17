a235896d8d4f077c289b9e0fff34a1f1ffb9aec8
Bylus <bylus@onet.eu>
2016-02-17 13:36:31 +0100

    Szymon

--- Pracownicy/src/pracownicy/Pracownicy.java
+++ Pracownicy/src/pracownicy/Pracownicy.java
@@ -15,7 +15,8 @@ public class Pracownicy {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        UserWindow userWindow = UserWindow.getInstance();
+        ChartWindow chartWindow = ChartWindow.getInstance();
     }
     
 }
